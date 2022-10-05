2a4f937106c64c77980c6ae8f3c5ee2e5914c129
marcelakor <sunchemarcel12@gmail.com>
2022-10-05 13:06:33 -0400

    first commit

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.12.1"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1" # define region as per your account
+}
+resource "aws_instance" "sunch" {
+  ami           = ami-0ee23bfc74a881de5
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_s3_bucket" "new_bucket" {
+  bucket = "demo-github-action-tf-medium"
+
+  object_lock_enabled = false
+
+  tags = {
+    Environment = "Prod"
+  }
+}
